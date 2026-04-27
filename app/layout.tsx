61f52b0b9ccb77c2b715596dbd1f4b3c3ca0d329
robertovillalobos996-rgb <robertovillalobos996@gmail.com>
2026-04-26 21:13:23 -0600

    Solución definitiva: Layout original restaurado con analítica aislada

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,27 +29,29 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {/* Radar puesto al final para que no mueva ni un pelo del diseño original */}
+        {/* Mantenemos su estructura original intacta */}
         {children}
 
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K6T9L8Q9"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-
-        <Script id="gtm-radar" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K6T9L8Q9');
-          `}
-        </Script>
+        {/* Bloque de Google aislado para que NO afecte el Flexbox ni las Cards */}
+        <div style={{ display: 'none', ariaHidden: true }}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-K6T9L8Q9"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K6T9L8Q9');
+            `}
+          </Script>
+        </div>
       </body>
     </html>
   );
